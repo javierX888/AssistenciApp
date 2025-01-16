4ce9235b816f05ce98d0c5c0b8ad1de735210f76
GianfrancoPuccio <gi.puccio@duocuc.cl>
2025-01-15 19:32:07 -0500

    Commit SRC
    Commit de SRC, ignorando el resto de las carpetas, usé Git Ignore

---
+++ src/app/cursosalumno/cursosalumno.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-cursosalumno',
+  templateUrl: './cursosalumno.page.html',
+  styleUrls: ['./cursosalumno.page.scss'],
+  standalone: true,
+  imports: [IonicModule]
+})
+export class CursosalumnoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/login/login.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LoginPageRoutingModule } from './login-routing.module';
+
+import { LoginPage } from './login.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LoginPageRoutingModule
+  ],
+  declarations: []//se deja vacío ya que el ionic modules es independiente
+})
+export class LoginPageModule {}
